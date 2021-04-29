1a82e6da92892ec18a7dd659ae53756710d6bceb
Joachim Protze <protze@itc.rwth-aachen.de>
2021-04-29 15:40:59 +0200

    Clang-format the C++ work queue example

--- detach-queue/test-task-detach-queue.cc
+++ detach-queue/test-task-detach-queue.cc
@@ -4,41 +4,43 @@
 #include <unistd.h>
 
 #include <algorithm>
-#include <ctime>
-#include <thread>
-#include <chrono>
-#include <mutex>
-#include <list>
-#include <iostream>
 #include <atomic>
+#include <chrono>
+#include <ctime>
+#include <iostream>
+#include <list>
+#include <mutex>
+#include <thread>
 
 using namespace std::literals::chrono_literals;
 
 struct WorkItem {
- omp_event_handle_t event;
- std::chrono::time_point<std::chrono::steady_clock> duetime;
- const int* a;
- int* b;
- int id;
- };
+  omp_event_handle_t event;
+  std::chrono::time_point<std::chrono::steady_clock> duetime;
+  const int *a;
+  int *b;
+  int id;
+};
 static std::mutex workQueueMtx;
 static std::list<WorkItem> workQueue;
 static std::atomic<bool> running{true};
 static std::atomic<int> workId{0};
 
-template<class R, class D>
-void queue_detach(const int* a, int* b, omp_event_handle_t event, std::chrono::duration<R,D> workLoad) {
-  WorkItem work{event, std::chrono::steady_clock::now() + workLoad, a, b, ++workId};
+template <class R, class D>
+void queue_detach(const int *a, int *b, omp_event_handle_t event,
+                  std::chrono::duration<R, D> workLoad) {
+  WorkItem work{event, std::chrono::steady_clock::now() + workLoad, a, b,
+                ++workId};
   std::unique_lock<std::mutex> lck(workQueueMtx);
   workQueue.emplace_back(work);
 }
 
-void run(void){
-  while(running){
+void run(void) {
+  while (running) {
     {
       std::unique_lock<std::mutex> lck(workQueueMtx);
-      while(!workQueue.empty() && workQueue.front().duetime < std::chrono::steady_clock::now() )
-      {
+      while (!workQueue.empty() &&
+             workQueue.front().duetime < std::chrono::steady_clock::now()) {
         *workQueue.front().b = *workQueue.front().a;
         std::cout << "Done with work " << workQueue.front().id << std::endl;
         omp_fulfill_event(workQueue.front().event);
@@ -49,12 +51,11 @@ void run(void){
   }
 }
 
-
 int main(int argc, char **argv) {
   int A[] = {1, 2, 3, 4, 5};
-  int B[] = {-1,-1,-1,-1,-1};
+  int B[] = {-1, -1, -1, -1, -1};
   omp_event_handle_t oevent, ievent;
-  
+
   std::thread runThread{run};
 
 #pragma omp parallel num_threads(12)
@@ -65,31 +66,27 @@ int main(int argc, char **argv) {
 #pragma omp task depend(inout : A[j], B[j]) detach(oevent)
       {
 #pragma omp task depend(inout : A[j], B[j]) detach(ievent)
-        queue_detach(A + j, B + j,
-                        ievent, 9ms);
+        queue_detach(A + j, B + j, ievent, 9ms);
 #pragma omp task depend(inout : A[j], B[j]) detach(ievent)
         {
           A[j] += B[j];
-        queue_detach(A + j, B + j,
-                        ievent, 5ms);
+          queue_detach(A + j, B + j, ievent, 5ms);
         }
 #pragma omp task depend(inout : A[j], B[j]) detach(ievent)
         {
           A[j] += B[j];
-        queue_detach(A + j, B + j,
-                        ievent, 11ms);
+          queue_detach(A + j, B + j, ievent, 11ms);
         }
 #pragma omp task depend(inout : A[j], B[j]) firstprivate(oevent)
         {
           A[j] += B[j];
-        queue_detach(A + j, B + j,
-                          oevent, 7ms);
+          queue_detach(A + j, B + j, oevent, 7ms);
         }
       }
     }
 #pragma omp taskwait
   }
   printf("%i, %i, %i, %i, %i\n", B[0], B[1], B[2], B[3], B[4]);
-  running=false;
+  running = false;
   runThread.join();
 }
